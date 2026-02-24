9e04aa330593ca66bf4ae2175f2b7103ae0e882e
nick <nick@legenex.com>
2026-02-24 12:42:47 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import Navbar from "@/components/landing/Navbar";
 
 export default function Layout({ children, currentPageName }) {
-  // Hide navbar on survey page
-  const hideNavbar = currentPageName === "Survey";
-
   return (
     <div>
-      {!hideNavbar && <Navbar />}
+      <Navbar />
       {children}
     </div>
   );
