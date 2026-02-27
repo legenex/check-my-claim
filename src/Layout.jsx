283e3d700be2a5da370b26bf5127c11e9ff8256e
nick <nick@legenex.com>
2026-02-27 10:46:53 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,6 +7,31 @@ export default function Layout({ children, currentPageName }) {
   const showNavbar = !NO_NAVBAR_PAGES.includes(currentPageName);
 
   useEffect(() => {
+    // Meta Pixel
+    const metaPixelScript = document.createElement("script");
+    metaPixelScript.innerHTML = `
+      function getEventIdFromUrl() {
+        const params = new URLSearchParams(window.location.search);
+        return params.get('event_id');
+      }
+      const eventId = getEventIdFromUrl();
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '764662699668489');
+      fbq('track', 'PageView', {}, {eventID: eventId});
+    `;
+    document.head.appendChild(metaPixelScript);
+
+    const noscript = document.createElement("noscript");
+    noscript.innerHTML = '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=764662699668489&ev=PageView&noscript=1"/>';
+    document.head.appendChild(noscript);
+
     const scripts = [
       { src: "https://static.truecall.com/c/x3co6aj15sabuvd33z06oli1cb4qzlyn979j.js" },
       { src: "https://static.truecall.com/s/truecall.js", id: "__tc_script", "data-campaign_uuid": "x3co6aj15sabuvd33z06oli1kb4qzlyn979j" },
